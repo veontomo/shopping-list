5e369cd4e47a513c749a28ae0a62c35f5c024fd8
Andrey Shcherbakov <veontomo@gmail.com>
2018-03-03 15:16:32 +0100

    Add the bootstrap to the dependencies

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CreateItemComponent } from './app/create-item/create-item.component';
+import { CreateItemComponent } from '../app/create-item/create-item.component';
 
 
 @NgModule({
