669eddc8bf3ec8e545a65853922fa830a9ae3f08
Andrey Shcherbakov <veontomo@gmail.com>
2018-03-04 21:06:11 +0100

    Feat: create a shopping cart component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CreateItemComponent } from './create-item/create-item.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateItemComponent
+    CreateItemComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule, FormsModule
