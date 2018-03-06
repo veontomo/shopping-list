f3864b403b4f393da2b62e7cdda78e34b1f310d9
Andrey Shcherbakov <veontomo@gmail.com>
2018-03-06 21:50:30 +0100

    Test: check the binding of price and name fields

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { AppComponent } from './app.component';
 import {CreateItemComponent} from './create-item/create-item.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -18,15 +20,19 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('shopping cart item');
   }));
+
+
 });
--- client/src/app/create-item/create-item.component.spec.ts
+++ client/src/app/create-item/create-item.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateItemComponent } from './create-item.component';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('CreateItemComponent', () => {
   let component: CreateItemComponent;
@@ -31,4 +32,31 @@ describe('CreateItemComponent', () => {
     const compiled = fixture.debugElement.query(By.css('.btn')).nativeElement;
     expect(compiled.textContent).toContain('Add');
   }));
+
+  it('should insert the item name', async(() => {
+    const nameElem: DebugElement = fixture.debugElement.query(By.css('#itemName'));
+    const nameNative = nameElem.nativeElement;
+    nameNative.value = 'test-product';
+    nameNative.dispatchEvent(new Event('input'));
+    expect(component.item.name).toBe('test-product');
+  }));
+
+  it('should insert the item price', async(() => {
+    const priceElem: DebugElement = fixture.debugElement.query(By.css('#itemPrice'));
+    const priceNative = priceElem.nativeElement;
+    priceNative.value = '123.22';
+    priceNative.dispatchEvent(new Event('input'));
+    expect(component.item.price).toBe(123.22);
+  }));
+
+  it('should insert the item price', async(() => {
+    const spy = spyOn(component, 'getCategories').and.returnValue(['first', 'second', 'third']);
+    const category = fixture.debugElement.query(By.css('#itemCategory')).nativeElement;
+    category.value = 2;
+    category.dispatchEvent(new Event('input'));
+    expect(component.item.category).toBe(2);
+  }));
+
+
+
 });
