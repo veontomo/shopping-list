60d97b119d7d1c2a10e383e2b0238ff31e20e22b
Andrey Shcherbakov <veontomo@gmail.com>
2018-03-10 15:13:10 +0100

    Feat: start using RxJava

--- server/src/main/java/it/neatec/server/controller/CategoryController.java
+++ server/src/main/java/it/neatec/server/controller/CategoryController.java
@@ -1,26 +1,22 @@
 package it.neatec.server.controller;
 
+import io.reactivex.Single;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Controller for retrieving categories.
- *
  * This is a "warm-up" code.
  */
 @RestController
 public class CategoryController {
     @GetMapping("/categories")
     @CrossOrigin(origins = "http://localhost:4200")
-    public List<String> getCategories(){
-        return new ArrayList<String>(){{
-            add("food");
-            add("household");
-            add("other");
-        }};
+    public Single<List<String>> getCategories() {
+        var data = List.of("food", "household", "other");
+        return Single.just(data);
     }
 }
