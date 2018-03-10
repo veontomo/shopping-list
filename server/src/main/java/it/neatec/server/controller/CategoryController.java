5d183fcc67305ed5f3e1ad2d472b53abbd4c525f
Andrey Shcherbakov <veontomo@gmail.com>
2018-03-10 10:31:11 +0100

    Fix: add a cross-origin filter to a back-end method /categories

--- server/src/main/java/it/neatec/server/controller/CategoryController.java
+++ server/src/main/java/it/neatec/server/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package it.neatec.server.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 public class CategoryController {
     @GetMapping("/categories")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<String> getCategories(){
         return new ArrayList<String>(){{
             add("food");
